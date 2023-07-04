0b2eceaefe693528b99194b8f3f85bdffab90b08
Argen <argenbagylov0@gmail.com>
2023-07-04 13:21:11 +0600

    example commit

---
+++ app/src/main/java/com/example/testunaamarket/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.testunaamarket
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.example.testunaamarket.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
